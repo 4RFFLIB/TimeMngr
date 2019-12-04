c76d315aad6965b791e5d9e2613846ab76267676
syed93uthman <syed_uthman@yahoo.com>
2019-12-04 14:48:31 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TimeMngr Library
+This repo is manage time using millis by make it simpler comparing to blink without delay.
---
+++ src/TimeMngr.h
@@ -0,0 +1,69 @@
+// myTimer.h
+
+#ifndef _MYTIMER_h
+#define _MYTIMER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "Signals.h"
+#include "Test.h"
+
+
+class MyTimer
+{
+ protected:
+	 unsigned long pMills = 0;
+	 unsigned long	interval;
+	 bool enable = false;
+
+ public:
+	Signals<> SignalTimeout;
+
+	//TestClass<void> TestSignal1;
+	//TestClass<int> TestSignal2;
+	//TestClass<int,int> TestSignal3;
+
+
+	MyTimer() {
+		enable = false;
+	}
+
+	MyTimer(unsigned long intervalMs, bool tmrStart)
+	{
+		interval = intervalMs;
+		pMills = millis();
+		enable = tmrStart;
+	}
+
+	void setInterval(unsigned long milisec) {
+		interval = milisec;
+	}
+
+	void start(void) {
+		enable = true;
+		pMills = millis();
+	}
+
+	void stop(void) {
+		enable = false;
+	}
+
+	void tick(void) {
+		if (enable) {
+			unsigned long cMills = millis();
+			if (cMills - pMills >= interval) {
+				pMills = cMills;
+				SignalTimeout.emit();
+			}
+		}
+	}
+};
+
+//extern MyTimerClass MyTimer;
+
+#endif
+
